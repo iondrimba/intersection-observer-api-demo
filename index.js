af1f42371f0eccf4e6f6f4f0ee217032fb295837
Ion D. Filho <iondrimba@gmail.com>
2018-06-09 18:29:15 -0300

    added legend

--- index.js
+++ index.js
@@ -1,65 +1,67 @@
 const getThreshold = () => {
   let data = [];
 
-  for (let i=0; i<=1.0; i+= 0.01) {
+  for (let i = 0; i <= 1.0; i += 0.01) {
     data.push(i);
   }
 
   return data;
 }
 
-const mountObserver = ({target, handler, threshold}) => {
+const mountObserver = ({ target, handler, threshold }) => {
   const options = {
     root: null,
     rootMargin: '0px',
     threshold
   }
 
-  const observer = new IntersectionObserver( handler, options);
+  const observer = new IntersectionObserver(handler, options);
 
   observer.observe(target);
 }
 
-mountObserver({
-  target: document.querySelector('.view--home'),
-  handler: (entries) =>{
-    entries.forEach(function(entry) {
-      console.log('entry', entry.target.className, (Math.floor(entry.intersectionRatio * 100)) + "%");
-    });
-  },
-  threshold: getThreshold()
-});
+const handleChanges = (entries) => {
+  entries.forEach(function ({ target, intersectionRatio }) {
+    const element = target.querySelector('.percentage__value');
+    const bg = target.querySelector('.legend__bg');
+    const view = target.className.split(' ')[1];
 
-mountObserver({
-  target: document.querySelector('.view--about'),
-  handler: (entries) =>{
-    entries.forEach(function(entry) {
-      console.log('entry', entry.target.className, (Math.floor(entry.intersectionRatio * 100)) + "%");
-    });
-  },
-  threshold: getThreshold()
-});
+    element.textContent = Math.round(intersectionRatio * 100);
 
-mountObserver({
-  target: document.querySelector('.view--contact'),
-  handler: (entries) =>{
-    entries.forEach(function(entry) {
-      console.log('entry', entry.target.className, (Math.floor(entry.intersectionRatio * 100)) + "%");
-    });
-  },
-  threshold: getThreshold()
-});
+    handleLegendBg({
+      bg: document.querySelector(`.legend__item--${view}`).querySelector('.legend__bg'),
+      percentage: Math.round(intersectionRatio * 100) /100
+    })
+  });
+}
 
-mountObserver({
-  target: document.querySelector('.view--services'),
-  handler: (entries) =>{
-    entries.forEach(function(entry) {
-      console.log('entry', entry.target.className, (Math.floor(entry.intersectionRatio * 100)) + "%");
-    });
-  },
-  threshold: getThreshold()
-});
+const handleLegendBg = ({ bg, percentage }) => {
+  console.log('handleLegendBg', percentage);
+  bg.style.transform = `scaleX(${percentage}`;
+};
 
-document.addEventListener('DOMContentLoaded', function(event) {
+document.addEventListener('DOMContentLoaded', function (event) {
+  mountObserver({
+    target: document.querySelector('.home'),
+    handler: handleChanges,
+    threshold: getThreshold()
+  });
 
+  mountObserver({
+    target: document.querySelector('.about'),
+    handler: handleChanges,
+    threshold: getThreshold()
+  });
+
+  mountObserver({
+    target: document.querySelector('.contact'),
+    handler: handleChanges,
+    threshold: getThreshold()
+  });
+
+  mountObserver({
+    target: document.querySelector('.services'),
+    handler: handleChanges,
+    threshold: getThreshold()
+  });
 });
